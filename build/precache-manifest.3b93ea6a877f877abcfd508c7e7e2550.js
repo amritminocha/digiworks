50423ac4779361ce2d561f722b73a7bc96a8d826
amritminocha <minochaamrit@gmail.com>
2021-01-08 01:32:00 +0530

    build

---
+++ build/precache-manifest.3b93ea6a877f877abcfd508c7e7e2550.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ef975e344ce4a520523f49d4b7ee74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11fbd29b7fd60810ed71",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1c3e66c403676d21f3e5",
+    "url": "/static/css/main.0f6206a5.chunk.css"
+  },
+  {
+    "revision": "11fbd29b7fd60810ed71",
+    "url": "/static/js/2.e0e7fa6d.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/static/js/2.e0e7fa6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c3e66c403676d21f3e5",
+    "url": "/static/js/main.4f978d41.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/static/media/login.9a76e74e.svg"
+  }
+]);
